d0b89abed365fb5228b84f00f8ab064be98205e7
Rohan Survase <rohansurvase10@gmail.com>
2023-02-18 18:04:49 +0530

    Quiz app completed

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,106 @@
+
+
+
+let questions = [{
+  numb: 1, question: "What does HTML stand for?", answer: "Hyper Text Markup Language", options: ["Hyper Text Preprocessor", "Hyper Text Markup Language", "Hyper Text Multiple Language", "Hyper Tool Multi Language"]
+},
+{
+  numb: 2,
+  question: "What does CSS stand for?",
+  answer: "Cascading Style Sheet",
+  options: [
+    "Common Style Sheet",
+    "Colorful Style Sheet",
+    "Computer Style Sheet",
+    "Cascading Style Sheet"
+  ]
+},
+{
+  numb: 3,
+  question: "What does PHP stand for?",
+  answer: "Hypertext Preprocessor",
+  options: [
+    "Hypertext Preprocessor",
+    "Hypertext Programming",
+    "Hypertext Preprogramming",
+    "Hometext Preprocessor"
+  ]
+},
+{
+  numb: 4,
+  question: "What does SQL stand for?",
+  answer: "Structured Query Language",
+  options: [
+    "Stylish Question Language",
+    "Stylesheet Query Language",
+    "Statement Question Language",
+    "Structured Query Language"
+  ]
+},
+{
+  numb: 5,
+  question: "What does XML stand for?",
+  answer: "eXtensible Markup Language",
+  options: [
+    "eXtensible Markup Language",
+    "eXecutable Multiple Language",
+    "eXTra Multi-Program Language",
+    "eXamine Multiple Language"
+  ]
+},
+{
+  numb: 6,
+  question: "What does HTTP stand for?",
+  answer: "Hypertext Transfer Protocol",
+  options: [
+    "Hypertext Transfer Protocol",
+    "Hypertext Translation Protocol",
+    "Hypertext Transmit Protocol",
+    "Hypertext Transaction Protocol"
+  ]
+},
+{
+  numb: 7,
+  question: "What does FTP stand for?",
+  answer: "File Transfer Protocol",
+  options: [
+    "File Transfer Protocol",
+    "File Transmission Protocol",
+    "File Translation Protocol",
+    "File Transaction Protocol"
+  ]
+},
+{
+  numb: 8,
+  question: "What does SSH stand for?",
+  answer: "Secure Shell",
+  options: [
+    "Secure Shell",
+    "Secure Server Host",
+    "Security Service Host",
+    "Server Secure Host"
+  ]
+},
+{
+  numb: 9,
+  question: "What does DNS stand for?",
+  answer: "Domain Name System",
+  options: [
+    "Domain Name Service",
+    "Domain Name System",
+    "Domain Name Server",
+    "Domain Name Security"
+  ]
+},
+{
+  numb: 10,
+  question: "What does GPU stand for?",
+  answer: "Graphics Processing Unit",
+  options: [
+    "Graphics Processing Unit",
+    "Graphics Power Unit",
+    "Graphics Performance Unit",
+    "Graphics Presentation Unit"
+  ]
+}
+];
